4c40a7bb86eaffc376d048955d5ffd2ac785fa9c
pancho <fppanchoo@gmail.com>
2025-01-19 00:18:35 +0100

    clean

--- Lines.py
+++ Lines.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import QGraphicsEllipseItem, QGraphicsRectItem, QApplication, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
 import PyQt5.QtGui
 from PyQt5.QtCore import Qt, QRectF
+from PyQt5.QtWidgets import QGraphicsEllipseItem, QGraphicsRectItem, QApplication, QGraphicsView, QGraphicsScene, \
+     QGraphicsPixmapItem
 
 app = QApplication([])
 
@@ -12,7 +13,7 @@ view.showFullScreen()
 # fetch screen definition
 screen = app.primaryScreen()
 screenGeometry = screen.geometry()
-view.setSceneRect(screenGeometry)  # Define scene size
+view.setSceneRect(QRectF(screenGeometry))  # Define scene size
 screenWidth = screenGeometry.width()
 screenHeight = screenGeometry.height()
 # y adapter la vue
@@ -54,67 +55,37 @@ def lineUpdate():
      pass
 
 rect1 = QGraphicsRectItem()
-rect1->setRect(QRectF((X1[0] * size().width() / 65535), (Y1[0] * size().height() / 65535), (X2[0] * size().width() / 65535), (Y2[0] * size().height() / 65535)));
-    rect1->setPen(PyQt5.QtGui.QPen(PyQt5.QtGui.QColor(rouge[0], vert[0], bleu[0], master[0]),
-                                   thickness[0],
-                                   Qt::SolidLine,
-                       Qt::RoundCap,
-                       Qt::RoundJoin));
-    rect1->setTransformOriginPoint(rect1->boundingRect().center());
-    rect1->setRotation(R[0]);
+rect1.setRect(QRectF((X1[0] * screenWidth / 65535), (Y1[0] * screenHeight / 65535), (X2[0] * screenWidth / 65535), (Y2[0] * screenHeight / 65535)))
+rect1.setPen(PyQt5.QtGui.QPen(PyQt5.QtGui.QColor(rouge[0], vert[0], bleu[0], master[0]), thickness[0], Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+rect1.setTransformOriginPoint(rect1.boundingRect().center())
+rect1.setRotation(R[0])
 
-    rect2 = QGraphicsRectangleItem()
-    rect2->setRect(
-        QRectF(((size().width() - X1[1] * size().width() / 65535) - X2[1] * size().width() / 65535),
-               ((size().height() - Y1[1] * size().height() / 65535)
-                - Y2[1] * size().height() / 65535),
-               (X2[1] * size().width() / 65535),
-               (Y2[1] * size().height() / 65535)));
-    rect2->setPen(PyQt5.QtGui.QPen(PyQt5.QtGui.QColor(rouge[1], vert[1], bleu[1], master[1]),
-                                   thickness[1],
-                                   Qt::SolidLine,
-                       Qt::RoundCap,
-                       Qt::RoundJoin));
-    rect2->setTransformOriginPoint(rect2->boundingRect().center());
-    rect2->setRotation(R[1]);
+rect2 = QGraphicsRectItem()
+rect2.setRect(QRectF(((screenWidth - X1[1] * screenWidth / 65535) - X2[1] * screenWidth / 65535),((screenHeight - Y1[1] * screenHeight / 65535) - Y2[1] * screenHeight / 65535), (X2[1] * screenWidth / 65535), (Y2[1] * screenHeight / 65535)))
+rect2.setPen(PyQt5.QtGui.QPen(PyQt5.QtGui.QColor(rouge[1], vert[1], bleu[1], master[1]), thickness[1], Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+rect2.setTransformOriginPoint(rect2.boundingRect().center())
+rect2.setRotation(R[1])
 
+ellipse1 = QGraphicsEllipseItem()
+ellipse1.setRect(QRectF((X1[2] * screenWidth / 65535), ((screenHeight - Y1[2] * screenHeight / 65535) - Y2[2] * screenHeight / 65535), (2 * X2[2] * screenWidth / 65535), (2 * Y2[2] * screenHeight / 65535)))
+ellipse1.setPen(PyQt5.QtGui.QPen(PyQt5.QtGui.QColor(rouge[2], vert[2], bleu[2], master[2]), thickness[2], Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+ellipse1.setTransformOriginPoint(ellipse1.boundingRect().center())
+ellipse1.setRotation(R[2])
 
-    ellipse1 = QGraphicsEllipseItem()
-    ellipse1->setRect(QRectF((X1[2] * size().width() / 65535),
-                             ((size().height() - Y1[2] * size().height() / 65535)
-                              - Y2[2] * size().height() / 65535),
-                             (2 * X2[2] * size().width() / 65535),
-                             (2 * Y2[2] * size().height() / 65535)));
-    ellipse1->setPen(PyQt5.QtGui.QPen(PyQt5.QtGui.QColor(rouge[2], vert[2], bleu[2], master[2]),
-                                      thickness[2],
-                                      Qt::SolidLine,
-                          Qt::RoundCap,
-                          Qt::RoundJoin));
-    ellipse1->setTransformOriginPoint(ellipse1->boundingRect().center());
-    ellipse1->setRotation(R[2]);
+ellipse2 = QGraphicsEllipseItem()
+ellipse2.setRect(QRectF((X1[3] * screenWidth / 65535), ((screenHeight - Y1[3] * screenHeight / 65535) - Y2[3] * screenHeight / 65535), (X2[3] * screenWidth / 65535), (Y2[3] * screenHeight / 65535)))
+ellipse2.setPen(PyQt5.QtGui.QPen(PyQt5.QtGui.QColor(rouge[3], vert[3], bleu[3], master[3]), thickness[3], Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+ellipse2.setTransformOriginPoint(ellipse2.boundingRect().center())
+ellipse2.setRotation(R[3])
 
-    ellipse2 = QGraphicsEllipseItem()
-    ellipse2->setRect(QRectF((X1[3] * size().width() / 65535),
-                             ((size().height() - Y1[3] * size().height() / 65535)
-                              - Y2[3] * size().height() / 65535),
-                             (X2[3] * size().width() / 65535),
-                             (Y2[3] * size().height() / 65535)));
-    ellipse2->setPen(PyQt5.QtGui.QPen(PyQt5.QtGui.QColor(rouge[3], vert[3], bleu[3], master[3]),
-                                      thickness[3],
-                                      Qt::SolidLine,
-                          Qt::RoundCap,
-                          Qt::RoundJoin));
-    ellipse2->setTransformOriginPoint(ellipse2->boundingRect().center());
-    ellipse2->setRotation(R[3]);
-
-    pix->setTransformationMode(Qt::SmoothTransformation);
-    float xPix = X1[4] * size().width();
-    float yPix = Y1[4] * size().height();
-    float hPix = X2[4] * size().width() / 127;
-    float vPix = Y2[4] * size().height() / 127;
-    pix->setOpacity(master[4] / 255);
-    QTransform trans;
-    trans.setMatrix(hPix / 65535, 0, 0, 0, vPix / 65535, 0, xPix / 65535, yPix / 65535, 1);
-    pix->setTransform(trans);
-    pix->setTransformOriginPoint(pix->boundingRect().center());
-    pix->setRotation(R[4]);
+pix.setTransformationMode(Qt.SmoothTransformation)
+xPix = X1[4] * screenWidth
+yPix = Y1[4] * screenHeight
+hPix = X2[4] * screenWidth / 127
+vPix = Y2[4] * screenHeight / 127
+pix.setOpacity(master[4] / 255)
+trans = PyQt5.QtGui.QTransform()
+trans.setMatrix(hPix / 65535, 0, 0, 0, vPix / 65535, 0, xPix / 65535, yPix / 65535, 1)
+pix.setTransform(trans)
+pix.setTransformOriginPoint(pix.boundingRect().center())
+pix.setRotation(R[4])
